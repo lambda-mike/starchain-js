b52bbf7f993788ab99672917bde6887485e78374
Michał Grygierzec <michal.grygierzec@protonmail.com>
2020-05-30 15:03:56 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -8,14 +8,14 @@ curl -s "localhost:8000/block/0"
 echo
 
 # TEST 2. Make request of ownership
-curl -s -X POST -H 'Content-Type: application/json' -d '{"address":"bc1qqgz2favl6mhz0awqrgskrm7km6sy89vfljpcz2"}' localhost:8000/requestValidation
+curl -s -X POST -H 'Content-Type: application/json' -d '{"address":"1DJhoaeJvEfPoT3AzUQNYHj5BFFycrLENW"}' localhost:8000/requestValidation
 echo
 
 # TEST 3. Submit your Star
 curl -s -X POST -H 'Content-Type: application/json' localhost:8000/submitstar -d @- <<\EOF
 { "address": "1DJhoaeJvEfPoT3AzUQNYHj5BFFycrLENW",
-  "signature": "HwUbL0skcaLQlsUMIgz1W5Tyn0u9LnJ18811jgeEw2OedyYrcMTaOlbR9Dx+auLTXMK0BEsvI14Hhk/+xGsHWQQ=",
-  "message": "1DJhoaeJvEfPoT3AzUQNYHj5BFFycrLENW:1590845723:starRegistry",
+  "signature": "IPcWVLdDGIrX0nwdlF0YqSsJajfkTCKQsw9aZ0x1lgJodBD8kS9x6OnJXR7zRj/cTUdv2aIG7L372tap3zkn6lM=",
+  "message": "1DJhoaeJvEfPoT3AzUQNYHj5BFFycrLENW:1590846424:starRegistry",
   "star": {
     "dec": "68° 52' 56.9",
     "ra": "16h 29m 1.0s",
